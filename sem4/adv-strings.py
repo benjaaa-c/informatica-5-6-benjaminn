cb715c5e002864b45b84fc2464ee7a9a0c7b1412
benjaaa-c <benjacl.sh4@gmail.com>
2025-09-02 16:15:56 +0000

    fttc

---
+++ sem4/adv-strings.py
@@ -0,0 +1,16 @@
+#String Length
+input_string = input("Please type a message: ")
+# print(input_string)
+
+# print("-" * len(input_string))
+
+# #String Index
+# print(input_string[0])
+# print(input_string[1])
+# print(input_string[-1])
+
+#Example
+i = 0
+while i < len(input_string):
+    print(input_string[i])
+    i += 1
