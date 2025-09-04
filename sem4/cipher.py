ff98d60a3c0d2f341ee921fc1d712a6c0917691f
benjaaa-c <benjacl.sh4@gmail.com>
2025-09-04 17:10:53 +0000

    ddd

--- sem4/cipher.py
+++ sem4/cipher.py
@@ -1,2 +1,23 @@
 def main():
     message = input("type a message: ").lower()
+    encode_message(message)
+
+
+def encode_message(text):
+    alphabet ="abcdefghijklmnopqrstuvwxyz"
+    cipher = "zywvutsrqponmlkjihgfedcba"
+    new_message = ""
+    i = 0
+
+    while i < len(text):
+        char = text[i]
+
+        if char in alphabet:
+            cipher_index = alphabet.find(char)
+            new_message += cipher[cipher_index]
+        else:
+            new_message += char
+        i += 1
+        print("Enconded message :" + new_message)
+
+main()
