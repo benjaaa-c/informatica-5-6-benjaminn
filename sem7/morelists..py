9ebbade7e27a42a3065e905d92aea1e1a22fc7e2
benjaaa-c <benjacl.sh4@gmail.com>
2025-09-22 19:39:56 +0000

    kkk

---
+++ sem7/morelists..py
@@ -0,0 +1,29 @@
+# #lists with strings
+# names = ["bob", "alex", "kevin"]
+# names.append("joseph")
+
+# for name in sorted(names):
+#     print(name)
+
+# # list with floats
+# measurements = [-2.5, 1.1, 7.5, 4.5, 21.5, 19.2]
+# mean = sum(measurements) / len(measurements)
+# print("the sum is:", mean)
+
+# # list within lists
+# super_list = [[5,2,3],[4,1],[2,2,5,1]]
+# print(super_list[1][0])
+
+# grades = [["Shakira", 8, "D"], ["Melissa", 0, "C"], ["Benjamin", 8, "B"]]
+# for student in grades:
+#     name = student[0]
+#     grade = student[1]
+#     group = student[2]
+#     print(f"(name) from group (group) got a (grade).")
+
+# matrices
+matrix = [[1,2,3], [4,5,6], [7,8,9]]
+# print rows
+for row in matrix:
+    print(row)
+    
